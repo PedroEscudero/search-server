ba295688fb6ce8e32a04b2a5c42baf738a8cca3e
Marc Morera <yuhu@mmoreram.com>
2017-11-16 19:49:03 +0100

    Testing FiniteServer in prod

---
+++ web/FiniteServer.php
@@ -0,0 +1,88 @@
+<?php
+
+use Evenement\EventEmitter;
+use React\Socket\ServerInterface;
+use React\Socket\ConnectionInterface;
+
+/**
+ * The `LimitingServer` decorator wraps a given `ServerInterface` and is responsible
+ * for limiting and keeping track of open connections to this server instance.
+ *
+ * Whenever the underlying server emits a `connection` event, it will check its
+ * limits and then either
+ * - keep track of this connection by adding it to the list of
+ *   open connections and then forward the `connection` event
+ * - or reject (close) the connection when its limits are exceeded and will
+ *   forward an `error` event instead.
+ *
+ * Whenever a connection closes, it will remove this connection from the list of
+ * open connections.
+ *
+ * ```php
+ * $server = new LimitingServer($server, 100);
+ * $server->on('connection', function (ConnectionInterface $connection) {
+ *     $connection->write('hello there!' . PHP_EOL);
+ *     …
+ * });
+ * ```
+ *
+ * See also the `ServerInterface` for more details.
+ *
+ * @see ServerInterface
+ * @see ConnectionInterface
+ */
+class LimitingServer extends EventEmitter implements ServerInterface
+{
+    private $server;
+    private $iterations = 0;
+    private $maxIterations;
+
+    /**
+     * @param ServerInterface $server
+     */
+    public function __construct(ServerInterface $server, $maxIterations)
+    {
+        $this->server = $server;
+        $this->maxIterations = $maxIterations;
+        $this->server->on('connection', array($this, 'handleConnection'));
+        $this->server->on('error', array($this, 'handleError'));
+    }
+
+    public function getAddress()
+    {
+        return $this->server->getAddress();
+    }
+
+    public function pause()
+    {
+        $this->server->pause();
+    }
+
+    public function resume()
+    {
+        $this->server->resume();
+    }
+
+    public function close()
+    {
+        $this->server->close();
+    }
+
+    /** @internal */
+    public function handleConnection(ConnectionInterface $connection)
+    {
+        $this->iterations++;
+        if ($this->iterations >= $this->maxIterations) {
+            $connection->on('close', function() {
+                $this->server->close();
+            });
+        }
+        $this->emit('connection', array($connection));
+    }
+
+    /** @internal */
+    public function handleError(\Exception $error)
+    {
+        $this->emit('error', array($error));
+    }
+}
--- web/server.php
+++ web/server.php
@@ -13,23 +13,20 @@
  * @author Marc Morera <yuhu@mmoreram.com>
  */
 
-
-function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-    throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
-}
-
-
 $environment = 'prod';
-$debug = false;
 require __DIR__ . '/../vendor/autoload.php';
+require 'FiniteServer.php';
 
+\Symfony\Component\Debug\ErrorHandler::register();
+\Symfony\Component\Debug\ExceptionHandler::register();
 
 $oneBundleAppConfig = new \OneBundleApp\App\OneBundleAppConfig(__DIR__ . '/../', $environment);
 $kernel = new \Mmoreram\BaseBundle\Kernel\BaseKernel(
     $oneBundleAppConfig->getBundles(),
     $oneBundleAppConfig->getConfig(),
     $oneBundleAppConfig->getRoutes(),
-    $environment, $debug,
+    $environment,
+    false,
     realpath(__DIR__ . '/../var')
 );
 
@@ -38,39 +35,57 @@ $kernel = new \Mmoreram\BaseBundle\Kernel\BaseKernel(
  */
 $loop = \React\EventLoop\Factory::create();
 $socket = new \React\Socket\Server($argv[1], $loop);
+$limitedServer = new LimitingServer($socket);
 $http = new \React\Http\Server(function (\Psr\Http\Message\ServerRequestInterface $request) use ($kernel) {
-    $method = $request->getMethod();
-    $headers = $request->getHeaders();
-    $query = $request->getQueryParams();
-    $content = $request->getBody();
-    $post = array();
-    if (in_array(strtoupper($method), array('POST', 'PUT', 'DELETE', 'PATCH')) &&
-        isset($headers['Content-Type']) && (0 === strpos($headers['Content-Type'], 'application/x-www-form-urlencoded'))
-    ) {
-        parse_str($content, $post);
-    }
-    $symfonyRequest = new \Symfony\Component\HttpFoundation\Request(
-        $query,
-        $post,
-        array(),
-        array(), // To get the cookies, we'll need to parse the headers
-        $request->getUploadedFiles(),
-        array(), // Server is partially filled a few lines below
-        $content
-    );
-    $symfonyRequest->setMethod($method);
-    $symfonyRequest->headers->replace($headers);
-    $symfonyRequest->server->set('REQUEST_URI', $request->getUri());
-    if (isset($headers['Host'])) {
-        $symfonyRequest->server->set('SERVER_NAME', explode(':', $headers['Host'][0]));
+
+    try {
+        $method = $request->getMethod();
+        $headers = $request->getHeaders();
+        $query = $request->getQueryParams();
+        $content = $request->getBody();
+        $post = array();
+        if (in_array(strtoupper($method), array('POST', 'PUT', 'DELETE', 'PATCH')) &&
+            isset($headers['Content-Type']) && (0 === strpos($headers['Content-Type'], 'application/x-www-form-urlencoded'))
+        ) {
+            parse_str($content, $post);
+        }
+        $symfonyRequest = new \Symfony\Component\HttpFoundation\Request(
+            $query,
+            $post,
+            array(),
+            array(), // To get the cookies, we'll need to parse the headers
+            $request->getUploadedFiles(),
+            array(), // Server is partially filled a few lines below
+            $content
+        );
+        $symfonyRequest->setMethod($method);
+        $symfonyRequest->headers->replace($headers);
+        $symfonyRequest->server->set('REQUEST_URI', $request->getUri());
+        if (isset($headers['Host'])) {
+            $symfonyRequest->server->set('SERVER_NAME', explode(':', $headers['Host'][0]));
+        }
+
+        $symfonyResponse = $kernel->handle($symfonyRequest);
+        $httpResponse = new \React\Http\Response(
+            $symfonyResponse->getStatusCode(),
+            $symfonyResponse->headers->all(),
+            $symfonyResponse->getContent()
+        );
+        $kernel->terminate($symfonyRequest, $symfonyResponse);
+
+    /**
+     * Catching errors and sending to syslog
+     */
+    } catch (\Exception $e) {
+        openlog("search-server", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+        syslog(LOG_ALERT, "[{$e->getFile()}] [{$e->getCode()}] ::: [{$e->getMessage()}]");
+        closelog();
+
+        throw $e;
     }
 
-    $symfonyResponse = $kernel->handle($symfonyRequest);
-    return new \React\Http\Response(
-        $symfonyResponse->getStatusCode(),
-        $symfonyResponse->headers->all(),
-        $symfonyResponse->getContent()
-    );
+    return $httpResponse;
 });
-$http->listen($socket);
+
+$http->listen($limitedServer);
 $loop->run();
