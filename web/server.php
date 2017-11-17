5aef10def46a48eeafa92a5c8771d88da34644b6
Marc Morera <yuhu@mmoreram.com>
2017-11-17 08:50:34 +0100

    Fixed syslog server message

--- web/server.php
+++ web/server.php
@@ -67,9 +67,8 @@ $http = new \React\Http\Server(function (\Psr\Http\Message\ServerRequestInterfac
         }
 
         openlog("search-server", LOG_PID, LOG_LOCAL0);
-        syslog(LOG_INFO, sprintf("[%s] ::: [%s]",
-            $symfonyRequest->getClientIp(),
-            $symfonyRequest->getRequestUri()
+        syslog(LOG_INFO, sprintf("::: [%s]",
+            urldecode($symfonyRequest->getRequestUri())
         ));
         closelog();
 
